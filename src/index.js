f3a72f2bedcf6f11a0a1395fd8641bf138d66b8b
Anton Prokopovich <antton.prokopovich@gmail.com>
2022-03-11 07:43:48 +0300

    feat: add js file

---
+++ src/index.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    console.log("Hello!");
+
+    //Tags
+    addTagsClickHandler();
+}
+
+const addTagsClickHandler = () => {
+    document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+        if (e.target.classList.contains('tag')) {
+            let clickedTag = e.target;
+            removeSelectedTags();
+            selectClickedTag(clickedTag);
+        }
+    });
+}
+
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.strategies__tags .tag');
+    console.log(tags);
+    tags.forEach(tag => {
+        tag.classList.remove('tag_selected');
+        tag.classList.add('tag_bordered');
+    });
+}
+
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+    clickedTag.classList.remove('tag_bordered');
+}
